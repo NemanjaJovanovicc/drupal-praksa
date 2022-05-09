8e7fb7d6e3d2e6c076daaee19f6ecb71f04e4903
DESKTOP-1ULF7AL\praksa <nemanjajov98@gmail.com>
2022-05-09 15:28:42 +0200

    #16 adding new controller and twig

---
+++ modules/custom/movie_reservation/movie_reservation.module.php
@@ -0,0 +1,12 @@
+<?php
+
+    function movie_reservation_theme($existing, $type, $theme, $path){
+        return array(
+            'information_movie'=>array(
+                'variables'=>array(
+                    'items'=>[],
+                    'title'=>''
+                )
+            )
+        );
+    }
--- modules/custom/movie_reservation/src/Controller/MovieReservationController.php
+++ modules/custom/movie_reservation/src/Controller/MovieReservationController.php
@@ -3,7 +3,7 @@
 namespace Drupal\movie_reservation\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-
+/*
 class MovieReservationController extends ControllerBase{
     public function movieReservation(){
         return [
@@ -11,5 +11,49 @@ class MovieReservationController extends ControllerBase{
             '#markup' => '<h3>Welcome to Movie Reservation Page</h3>'
         ];
     }
+
+    public function information(){
+        $data = array(
+            'name'=>'Ime filma',
+            'description'=>'Opis filma'
+        );
+
+        return [
+            '#title'=>'Information Page, Movie Reservation',
+            '#theme'=>'information_movie',
+            '#items'=>$data
+        ];
+    }
 }
+*/
+class MovieReservationController {
+
+    public function showAllMovies()
+    {
+        $movies = [
+            ['title' => 'Movie1'],
+            ['title' => 'Movie2'],
+        ];
+
+        return [
+            '#theme' => 'movie-reservation',
+            '#movies' => $movies,
+            '#title' => 'Movies'
+        ];
+        $query = \Drupal::entityQuery('node_field_data')
+          ->condition('type', 'article');
+        $movies = \Drupal\node\Entity\Node::loadMultiple($query->execute());
+
+        return array(
+            '#theme' => 'movie-reservation',
+            '#movies' => $movies,
+            '#title' => 'Welcome to Movie Reservation Page'
+        );
+    }
+}
+
+
+
+
+
 ?>
