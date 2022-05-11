ea12edce2936abd08f56ffd85df5399de8f16f35
DESKTOP-1ULF7AL\praksa <nemanjajov98@gmail.com>
2022-05-11 09:37:43 +0200

    16-using-custom-controller-to-return-all-movies

--- modules/custom/movie_reservation/src/Controller/MovieReservationController.php
+++ modules/custom/movie_reservation/src/Controller/MovieReservationController.php
@@ -7,17 +7,14 @@ class MovieReservationController{
     public function movieReservation() {
    
       $query = \Drupal::entityQuery('node')
-        //->condition('type', 4);
         ->condition('type','movie');
       $movies = \Drupal\node\Entity\Node::loadMultiple($query->execute());
 
-      $c = 2;
        return array(
           '#theme' => 'information-movie',
           '#movies' => $movies,
           '#title' => 'Welcome to our movie reservation page'
         );
-
     }
   }
 ?>
